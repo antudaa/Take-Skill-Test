9095fb5c66de4264728f24d3adbee3b23ba35043
Antu_Das <antuchandradasp9@gmail.com>
2022-10-11 20:35:31 -0700

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>antu</h1>
-      {/* <header className="App-header">
-        <h1>Antu</h1>
-      </header> */}
+    <div>
+      <Header></Header>
+      <h1 className="text-5xl text-center font-bold underline">
+        Hello world!
+      </h1>
     </div>
   );
 }
